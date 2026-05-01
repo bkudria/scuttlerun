6cf97e9722abd41b0a08b9d6f4faf5e523ac5377
Benjamin Kudria <ben@kudria.net>
2026-05-01 08:08:51 -0700

    chore(format): add Prettier with ESLint integration
    Adds Prettier with project-standard config, an ignore list, format
    scripts, and eslint-config-prettier so ESLint stops fighting Prettier
    on stylistic rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier/flat";
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier,
   {
     ignores: ["dist/", "coverage/"],
   },
